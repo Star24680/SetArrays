2de963df3898e4e00184058082884525b4eef70f
Star24680 <gulerumaan1@gmail.com>
2024-11-06 17:47:16 +0500

    first commit

---
+++ SetOperation.py
@@ -0,0 +1,13 @@
+X={1,2,3}
+print (X)
+Y={1.0,"Hello",(1,2,3)}
+print(Y)
+Z={1,2,3,4,3,2}
+print(Z)
+A=set([1,2,3,4])
+print(A)
+B=set([0,1,2,3,4,5])
+print("Original Set")
+print(B)
+B.pop()
+print("After removing first element: ", B)
